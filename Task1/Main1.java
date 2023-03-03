cc83743c3bbf1b8e6b3e76690a50d30f3dd8af4d
CarolineLykke <carolinelh@live.dk>
2023-03-03 09:14:53 +0100

    add

--- Task1/Main1.java
+++ Task1/Main1.java
@@ -1,26 +1,31 @@
 class Main1 {
     public static void main(String[] args) {
         // create a team and set its rank
-        Team1 team1 = new Team1("Example Team 1");
+        Team1 team1 = new Team1("Team 1");
         team1.setRank(1);
 
         // print the team's name and rank
         System.out.println(team1);
 
         // create 5 more teams and print them
-        Team1 team2 = new Team1("Example Team 2");
+        Team1 team2 = new Team1("Team 2");
+        team2.setRank(3);
         System.out.println(team2);
 
-        Team1 team3 = new Team1("Example Team 3");
+        Team1 team3 = new Team1("Team 3");
+        team3.setRank(5);
         System.out.println(team3);
 
-        Team1 team4 = new Team1("Example Team 4");
+        Team1 team4 = new Team1("Team 4");
+        team4.setRank(5);
         System.out.println(team4);
 
-        Team1 team5 = new Team1("Example Team 5");
+        Team1 team5 = new Team1("Team 5");
+        team5.setRank(7);
         System.out.println(team5);
 
-        Team1 team6 = new Team1("Example Team 6");
+        Team1 team6 = new Team1("Team 6");
+        team6.setRank(2);
         System.out.println(team6);
     }
 }
--- Task1/Team1.java
+++ Task1/Team1.java
@@ -3,6 +3,30 @@ class Team1 {
     private int rank;
     private String[] players;
 
+    // Step 1.f
+    public Team1(String name) {
+        this.name = name;
+    }
+
+    // Step 1.h
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    // Step 1.j
+    @Override
+    public String toString() {
+        //skriver super. så jeg kan nedavle hvor den er blevet placeret
+        return super.toString() + "Team Name: " + this.name + " Rank: " + this.rank;
+    }
+}
+
+
+/*class Team1 {
+    private String name;
+    private int rank;
+    private String[] players;
+
     public Team1(String name) {
         this.name = name;
     }
@@ -26,4 +50,6 @@ class Team1 {
     public void setPlayers(String[] players) {
         this.players = players;
     }
+
 }
+*/
